8254bc2a2a328847b8d45c3f56c760bf4bf84f41
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-12-20 10:22:33 +0700

    Pembuatan konfigurasi ESLint dari Traversy Media

---
+++ index.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const x = 1;
+
+class UniClass {
+  helloWorld() {
+    console.log('hello world');
+  }
+}
+
+const testHello = new UniClass();
+testHello.helloWorld();
